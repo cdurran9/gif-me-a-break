9b3925d867a0a58751e87b7ce21276e29ac054fd
Carter Durrant <carter.durrant@gmail.com>
2021-12-15 23:53:07 -0700

    Created react client

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [message, setMessage] = useState('');
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch('/api');
+      const data = await response.json();
+      setMessage(data.message);
+    }
+    getData();
+  }, [])
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {message}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
